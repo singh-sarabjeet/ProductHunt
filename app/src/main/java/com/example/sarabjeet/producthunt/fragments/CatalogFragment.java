6be230c9bfb3c718375a7af30e4047e997e9de31
sarabjeet <sjsingh200893@gmail.com>
2017-04-12 01:12:16 +0530

    Added Catalog Fragment

--- app/src/main/java/com/example/sarabjeet/producthunt/fragments/CatalogFragment.java
+++ app/src/main/java/com/example/sarabjeet/producthunt/fragments/CatalogFragment.java
@@ -2,9 +2,16 @@ package com.example.sarabjeet.producthunt.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.sarabjeet.producthunt.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 /**
  * Created by sarabjeet on 11/4/17.
@@ -12,14 +19,18 @@ import android.view.ViewGroup;
 
 public class CatalogFragment extends Fragment {
 
+    @BindView(R.id.product_recycler_view)
+    RecyclerView gridRecyclerView;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
+        ButterKnife.bind(getActivity());
         return null;
     }
 }
