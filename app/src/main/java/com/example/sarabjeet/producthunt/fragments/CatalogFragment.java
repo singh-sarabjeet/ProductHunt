ea5eb06f7a832b9e93b21ca4bb8fbedb7e00b7bc
sarabjeet <sjsingh200893@gmail.com>
2017-04-12 00:58:03 +0530

    Added Catalog Fragment

---
+++ app/src/main/java/com/example/sarabjeet/producthunt/fragments/CatalogFragment.java
@@ -0,0 +1,25 @@
+package com.example.sarabjeet.producthunt.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by sarabjeet on 11/4/17.
+ */
+
+public class CatalogFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return null;
+    }
+}
